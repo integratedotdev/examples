b9c1087aef6979765bb024d51734598a449b4bc6
Jeremy Bosma <jectaunscripted@gmail.com>
2025-11-15 22:49:46 +0100

    automatic imports

--- nuxt-example/app/lib/integrate-server.ts
+++ nuxt-example/app/lib/integrate-server.ts
@@ -4,8 +4,6 @@ export const { client: serverClient, handler } = createMCPServer({
     apiKey: import.meta.env.INTEGRATE_API_KEY,
     plugins: [
         githubPlugin({
-            clientId: import.meta.env.GITHUB_CLIENT_ID,
-            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
             scopes: ['repo', 'user'],
         }),
     ],
