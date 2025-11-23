0cf45a4165623603ee2146786259cd1e046ff60e
Jeremy Bosma <jectaunscripted@gmail.com>
2025-11-23 03:12:56 +0100

    new folder structure

---
+++ frameworks/backend/elysia-example/src/index.ts
@@ -0,0 +1,27 @@
+import { Elysia } from "elysia";
+import { createMCPServer, githubIntegration } from "integrate-sdk/server";
+import { cors } from "@elysiajs/cors";
+
+const app = new Elysia();
+
+const { client: serverClient } = createMCPServer({
+  apiKey: process.env.INTEGRATE_API_KEY,
+  integrations: [
+    githubIntegration({
+      scopes: ["repo", "user"],
+    }),
+  ],
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.all("/api/integrate/*", (context) => {
+  return serverClient.handler(context.request);
+});
+
+app.listen(8080);
